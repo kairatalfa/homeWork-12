7539aeb14728bdb94a705609d60537b2023536bf
kairat <kairatkubanychbekov96@gmail.com>
2022-11-01 10:00:12 +0300

    first commit

---
+++ src/components/Meals/MealsItem/MealItem.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import classes from "./MealItem.module.css";
+export function MealItem(props) {
+  const price = props.price.toFixed(2);
+  const [state, setState] = useState(0);
+  const [total, setTotal] = useState(price);
+
+  function sushiMinus() {
+    if (state > 0) {
+      setState(state - 1);
+      setTotal((prevTotal) => +prevTotal - props.price);
+    }
+  }
+  function sushiplus() {
+    setState(state + 1);
+
+    setTotal((prevTotal) => +prevTotal + props.price);
+  }
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <p className={classes.description}>{props.description}</p>
+        <p className={classes.price}>{price}</p>
+      </div>
+      <div className={classes.minus}>
+        <button className={classes.click} onClick={sushiMinus}>
+          -
+        </button>
+        <p className={classes.total}>{state}</p>
+
+        <button className={classes.click} onClick={sushiplus}>
+          +
+        </button>
+        <div>
+          <p className={classes.total}>{Number(total).toFixed(2)}</p>
+        </div>
+      </div>
+    </li>
+  );
+}
